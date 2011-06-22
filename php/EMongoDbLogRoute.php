05d4ec329b3c0f81a36cc11e181d13e6fb242b78
aoyagikouhei <aoyagi.kouhei@gmail.com>
2011-06-23 07:15:28 +0900

    modify tab

--- php/EMongoDbLogRoute.php
+++ php/EMongoDbLogRoute.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * auther aoyagikouhei
+ * Auther aoyagikouhei
  * 2011/06/23 ver 1.0
  *
  * Install
@@ -8,10 +8,10 @@
  * 
  * In config/main.php:
   'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'ext.EMongoDbLogRoute',
+      'class'=>'CLogRouter',
+      'routes'=>array(
+        array(
+          'class'=>'ext.EMongoDbLogRoute',
           'levels'=>'trace, info, error, warning',
           'categories' => 'system.*',
         ),
@@ -30,10 +30,10 @@
  *
  * example
    'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'ext.EMongoDbLogRoute',
+      'class'=>'CLogRouter',
+      'routes'=>array(
+        array(
+          'class'=>'ext.EMongoDbLogRoute',
           'levels'=>'trace, info, error, warning',
           'categories' => 'system.*',
           'connectionString' => 'localhost:27017',
@@ -51,71 +51,71 @@
  */
 class EMongoDbLogRoute extends CLogRoute
 {
-	/**
-	 * @var string Mongo Db host + port
-	 */
+  /**
+   * @var string Mongo Db host + port
+   */
   public $connectionString="localhost:27017";
 
-	/**
-	 * @var string Mongo Db Name
-	 */
+  /**
+   * @var string Mongo Db Name
+   */
   public $dbName="test";
   
   /**
-	 * @var string Collection name
-	 */
+   * @var string Collection name
+   */
   public $collectionName="yiilog";
 
   /**
-	 * @var string message column name
-	 */
+   * @var string message column name
+   */
   public $message="message";
 
   /**
-	 * @var string level column name
-	 */
+   * @var string level column name
+   */
   public $level="level";
 
   /**
-	 * @var string category column name
-	 */
+   * @var string category column name
+   */
   public $category="category";
 
   /**
-	 * @var string timestamp column name
-	 */
+   * @var string timestamp column name
+   */
   public $timestamp="timestamp";
 
   /**
-	 * @var string timestamp type name float or date
-	 */
+   * @var string timestamp type name float or date
+   */
   public $timestampType="float";
 
-	/**
-	/**
-	 * @var Mongo mongo Db collection
-	 */
-	private $collection;
-	 
-	/**
-	 * Initializes the route.
-	 * This method is invoked after the route is created by the route manager.
-	 */
-	public function init()
-	{
+  /**
+  /**
+   * @var Mongo mongo Db collection
+   */
+  private $collection;
+   
+  /**
+   * Initializes the route.
+   * This method is invoked after the route is created by the route manager.
+   */
+  public function init()
+  {
     parent::init();
     $connection = new Mongo($this->connectionString);
     $dbName = $this->dbName;
     $collectionName = $this->collectionName;
     $this->collection = $connection->$dbName->$collectionName;
-	}
+  }
 
-	/**
-	 * Saves log messages into mongodb.
-	 * @param array list of log messages
-	 */
-	protected function processLogs($logs)
-	{
+  /**
+   * Saves log messages into mongodb.
+   * @param array list of log messages
+   */
+  protected function processLogs($logs)
+  {
     foreach($logs as $log) {
       $this->collection->insert(array(
         $this->message => $log[0]
@@ -124,7 +124,7 @@ class EMongoDbLogRoute extends CLogRoute
         ,$this->timestamp => 
           'date' === $this->timestampType ? new MongoDate(round($log[3])) : $log[3]
       ));
-		}
-	}
+    }
+  }
 
 }
